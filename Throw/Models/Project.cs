f89aed5ec454679e2c251a9e708cae39611e348c
Aleksandra Kovacevic <aleksandrakovacevic099@gmail.com>
2019-01-12 21:12:36 +0100

    Ukljucen Entity Framework, napravljena SQL baza, tabele mapirane na klase ,definisano  osnovno rutiranje i logovanje kao i  upisivanje korisnika.

---
+++ Throw/Models/Project.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Throw.Models
+{
+    public partial class Project
+    {
+        public Project()
+        {
+            ProjectSnapshot = new HashSet<ProjectSnapshot>();
+            UserProject = new HashSet<UserProject>();
+        }
+
+        public int Idproject { get; set; }
+        public string Name { get; set; }
+        public string Link { get; set; }
+
+        public ICollection<ProjectSnapshot> ProjectSnapshot { get; set; }
+        public ICollection<UserProject> UserProject { get; set; }
+    }
+}
--- Throw/Program.cs
+++ Throw/Program.cs
@@ -19,6 +19,6 @@ namespace Throw
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>().UseUrls("http://localhost:51529");
     }
 }
